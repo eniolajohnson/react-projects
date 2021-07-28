cef46f3252d37ee88e0455eacad16a184611d297
Eniola Johnson <writingincodes@gmail.com>
2021-07-28 22:10:05 +0100

    Added basic structure to birthday reminder src from what I learnt today

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const handleClick = () => {
+    setPeople([]);
+  }
+  return <div>
+    <h3>{people.length} people have their  birthdays today</h3>
+    <List people={people}/>
+    <button onClick={handleClick}>
+      clear all</button>
+  </div>;
 }
 
 export default App;
