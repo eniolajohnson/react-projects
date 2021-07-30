f94b187a90261860cdd0fc0afe50d703437a4899
Eniola Johnson <writingincodes@gmail.com>
2021-07-30 17:59:55 +0100

    fetched the api data using async await

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,38 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      setLoading(false);
+      setTours(tours);
+    } catch(error){
+      console.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  return (
+    <>
+      { loading 
+      ? <main>
+        <Loading />
+      </main> 
+      : 
+      <div>
+        
+      </div>
+      }
+    </>
+  )
 }
 
 export default App
