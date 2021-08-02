e34f10a3b7917ca45070937749b35337809e5f37
Eniola Johnson <writingincodes@gmail.com>
2021-08-02 17:06:24 +0100

    completed reviews project

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -5,30 +5,61 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, id, job, image, text } = people[index];
-  const maxLimit = people.length - 1;
+  // const maxLimit = people.length - 1;
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  }
 
   const handlePrev = () => {
-    const prevIndex = id - 2;
-    if (prevIndex > -1) {
-      setIndex(prevIndex);
-    } else {
-      setIndex(maxLimit)
-    }   
+    // const prevIndex = id - 2;
+    // if (prevIndex > -1) {
+    //   setIndex(prevIndex);
+    // } else {
+    //   setIndex(maxLimit)
+    // }  
+
+    // Shorter implementation
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    })
   }
 
   const handleNext = () => {
-    const nextIndex = id;
-    if (nextIndex < maxLimit) {
-      setIndex(nextIndex);
-    } else {
-      setIndex(0)
-    }   
+    // const nextIndex = id;
+    // if (nextIndex < maxLimit) {
+    //   setIndex(nextIndex);
+    // } else {
+    //   setIndex(0)
+    // }   
+
+    // Shorter implementation
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    })
   }
 
   const handleRandom = () => {
-    const randomNum = Math.round(Math.random() * 3);
-    console.log(randomNum);
-    setIndex(randomNum);
+    // const randomNum = Math.round(Math.random() * 3);
+    // setIndex(randomNum);
+
+    // Shorter implementation
+    setIndex((index) => {
+      let randomNum = Math.floor(Math.random() * people.length);
+      // Deal with repetition when making numbers random
+      if (randomNum === index) {
+        randomNum = index + 1;
+      }
+      return checkNumber(randomNum);
+    })
   }
 
   return (
