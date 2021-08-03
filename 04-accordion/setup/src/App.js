9887e17d3b7ae73f13be04e9ca6b2fef026605f1
Eniola Johnson <writingincodes@gmail.com>
2021-08-03 15:36:50 +0100

    implemented my solution for the accordion project

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [dataArr, setDataArr] = useState([]);
+
+  useEffect(() => {
+    setDataArr(data);
+  }, []);
+
+  return (
+    <main className='container'>
+        <h2>questions and answers about login</h2>
+        {dataArr.map((dataItem) => {
+          const { id } = dataItem;
+          return (
+            <SingleQuestion key={id} {...dataItem}/>
+          )
+        })}
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ id, info, title }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  const handleReadMore = () => {
+    setReadMore(!readMore);
+  }
+  return (
+    <div className="question">
+      { 
+        readMore 
+        ? <div>
+          <header>
+            <h3 className='title'>{title}</h3>
+            <button onClick={handleReadMore} className="btn">
+              <AiOutlineMinus />
+            </button>
+          </header>
+          <p className='question'>{info}</p>
+        </div>
+        : <div>
+          <header>
+          <h3 className='title'>{title}</h3>
+          <button onClick={handleReadMore} className="btn">
+            <AiOutlinePlus />
+          </button>
+          </header>
+        </div>
+    }
+    </div>
+  );
 };
 
 export default Question;
