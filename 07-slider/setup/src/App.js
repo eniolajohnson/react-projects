a76a1815ca1a5e6a70815dac4a488a2d18711825
eniolajohnson <writingincodes@gmail.com>
2021-08-23 17:03:06 +0100

    first commit

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -2,8 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+
 function App() {
-  return <h2>slider project setup</h2>;
+  const [ people, setPeople ] = useState(data);
+
+
+  return (
+    <main>
+      <h2>/ reviews</h2>
+    </main>
+  );
 }
 
 export default App;
