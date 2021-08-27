d90ebf9b3372ce155873baba7612b8e30b7e4f3b
eniolajohnson <writingincodes@gmail.com>
2021-08-27 17:15:41 +0100

    included conditional statements for css class change

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
@@ -19,8 +19,19 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex ) => {
           const { id, title, image, name, quote } = person;
+          let position = "nextSlide";
+
+          if (index === personIndex) {
+            position = "activeSlide"
+          }
+
+          if (personIndex === index - 1 || 
+            (index === 0 && personIndex === people.length - 1 )) {
+            position = "lastSlide"
+          }
+
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className='title'>{title}</p>
