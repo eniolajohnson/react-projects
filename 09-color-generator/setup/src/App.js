8b65437a3c66271ec4b54f792c8f4e1a63179b62
eniolajohnson <writingincodes@gmail.com>
2021-09-20 17:20:10 +0100

    completed the color-picker

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js';
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#800020').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,7 +24,7 @@ function App() {
       <form onSubmit={handleSubmit}>
         <input type="text" 
           value={color} 
-          placeholder='#ffffff'
+          placeholder='#800020'
           onChange={(e) => {setColor(e.target.value)}} />
         <button className="btn" type='submit'>submit</button>
       </form>
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -5,12 +5,28 @@ const SingleColor = ({ rgb, weight, index, hexColor}) => {
   const [alert, setAlert] = useState(false);
   const bgColor = rgb.join(',');
   const hex = rgbToHex(...rgb);
-  const hexValue = `#${hexColor}`
+  const hexValue = `#${hexColor}`;
+
+  const handleClick = (e) => {
+    setAlert(true);
+    navigator.clipboard.writeText(hex);
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(()=> {
+      setAlert(false)
+    }, 1500)
+    return () => clearTimeout(timeout);
+  }, [alert])
    
   return (
-    <article className={`color ${index > 10 && 'color-light'}`} style={{ backgroundColor: `rgb(${bgColor})` }}>
+    <article className={`color ${index > 10 && 'color-light'}`} style={{ backgroundColor: `rgb(${bgColor})` }} onClick={handleClick}>
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && 
+        <p className='alert'>
+          copied to clipboard
+        </p>}
     </article>
   )
 }
