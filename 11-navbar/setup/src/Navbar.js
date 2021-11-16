e9603da363a9119d656e8acb9c33cab1a36e983a
eniolajohnson <writingincodes@gmail.com>
2021-11-16 22:13:38 +0100

    wip

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,43 +4,41 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={()=>{
+            setShowLinks(!showLinks)
+          }}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
-            {links.map((link) => {
-              const {id, url, text} = link;
-              return (
-                <li key={id}>
-                  <a href={url}>{text}</a>
-                </li>
-                )
-            })}
-          </ul>
-        </div>
+        {showLinks && (
+          <div className="links-container show-container">
+            <ul className="links">
+              {links.map((link) => {
+                const {id, url, text} = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                  )
+              })}
+            </ul>
+          </div>
+        )}
         <ul className="social-icons">
-          <li>
-            <a href="https://www.twitter.com">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com">
-              <FaTwitter />
-            </a>
-          </li>
+          {social.map((socialIcon) => {
+            const {id, url, icon} = socialIcon;
+            <li key={id}>
+              <a href={url}>
+                {icon}
+              </a>
+            </li>
+          })}
         </ul>
       </div>
     </nav>
